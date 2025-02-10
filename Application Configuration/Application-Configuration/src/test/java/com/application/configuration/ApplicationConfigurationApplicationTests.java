ac2cdb9e9b541a8e9767350d0219b39cf81bc46e
loginmycode <rajreddy402@gmail.com>
2025-02-10 16:28:28 +0530

    Application-Confiuration

---
+++ Application Configuration/Application-Configuration/src/test/java/com/application/configuration/ApplicationConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
